7c64792ac5bb0544ef283d95652fdec1db87212a
zoufishanmehdi <zzmehdi@gmail.com>
2016-04-13 22:47:36 -0400

    meme

---
+++ Meme-ify/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Meme-ify
+//
+//  Created by Zoufishan Mehdi on 4/13/16.
+//  Copyright © 2016 c4q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
+
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            cameraButton.enabled = true
+        } else {
+            cameraButton.enabled = false
+        }
+    }
+
+    @IBAction func pickAnImageAlbum(sender: AnyObject) {
+        let imagePicker = UIImagePickerController ()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func pickAnImageCamera(sender: AnyObject) {
+        let imagePicker = UIImagePickerController ()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    // MARK: - UIImagePickerControllerDelegate Methods
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.contentMode = .ScaleAspectFit
+            imagePickerView.image = pickedImage
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
+
